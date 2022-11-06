b02d7d462bf5676e2eee558702ad0b127aa1128b
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-11-06 11:20:08 +0530

    Create gm1.php

---
+++ Good Morning/gm1.php
@@ -0,0 +1,20 @@
+
+// good morning
+     
+echo "🌷🌹🍃🌹🌷G \n";
+echo "┏╮╭┓┏╮╭┓ O \n";
+echo "╰╭╮╯╰╭╮╯ O \n";
+echo "╭╰╯╮╭╰╯╮ D \n";
+echo "┗╯╰┛┗╯╰┛  \n";
+echo "🌷🌹🍃🌹🌷 M \n";
+echo "┏╮╭┓┏╮╭┓  O \n";
+echo "╰╭╮╯╰╭╮╯  R \n";
+echo "╭╰╯╮╭╰╯╮  N \n";
+echo "┗╯╰┛┗╯╰┛   I \n";
+echo "🌷🌹🍃🌹🌷 N \n";
+echo "┏╮╭┓┏╮╭┓  G \n";
+echo "╰╭╮╯╰╭╮╯   \n";
+echo "╭╰╯╮╭╰╯╮  D \n";
+echo "┗╯╰┛┗╯╰┛  E \n";
+echo "🌷🌹🍃🌹🌷 A \n";
+echo "      FRIEND      \n";
